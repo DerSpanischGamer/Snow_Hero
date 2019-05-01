cc7517e70685ae66628feed7bc4f03f995d21532
DerSpanischGamer <robertogc99@hotmail.com>
2019-05-01 11:15:27 +0200

    kfjbk

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-import Threading
 from queue import Queue
 import time
 
@@ -11,13 +10,6 @@ frame = Frame(root, width = largueur, height = ancheur)
 
 def key(e):
     print("pressed", e.char)
-
-print_lock = threading.Lock()
-q = Queue()
-
-for x in range(10):
-    t = threading.Thread(target = threader)
-    
 
 frame.bind("<KeyPress>", key)        # detecte la touche que l'utilisateur a appuye
 frame.pack()
